7d193a8c431a529ae127ba132d3af9ae7ee3853d
JieKe <1726144041@qq.com>
2016-05-24 18:01:41 +0800

    commit code

---
+++ uiwebview/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  uiwebview
+//
+//  Created by 李双 on 16/2/2.
+//  Copyright © 2016年 YouKa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+
+@end
+
---
+++ uiwebview/ViewController.m
@@ -0,0 +1,56 @@
+//
+//  ViewController.m
+//  uiwebview
+//
+//  Created by 李双 on 16/2/2.
+//  Copyright © 2016年 YouKa. All rights reserved.
+//
+
+#import "ViewController.h"
+static NSString* commonHtmlTitle = @"<font size=\"10\">";
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+//- (id)init{
+//    if (self = [super initWithNibName:@"viewcontroller" bundle:nil]) {
+//        
+//    }
+//    return self;
+//}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"viewDidLoad");
+    CGRect frame = CGRectMake(50,50,300,300);
+//    frame.size = [UIImage imageNamed:@"guzhang.gif"].size;
+    // 读取gif图片数据
+    NSData *gif = [NSData dataWithContentsOfFile: [[NSBundle mainBundle] pathForResource:@"guzhang" ofType:@"gif"]];
+    // view生成
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:frame];
+    webView.userInteractionEnabled = NO;//用户不可交互
+    [webView loadData:gif MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
+//    [webView loadData:gif MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
+    [self.view addSubview:webView];
+
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    NSLog(@"recogniseBtn");
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)recognise:(UIButton *)sender {
+//    NSMutableString* htmlString = [NSMutableString stringWithString:commonHtmlTitle];
+//    [htmlString appendString:@""];
+//    
+//    [_webView loadHTMLString:htmlString baseURL:nil];
+}
+- (IBAction)clearView:(UIButton *)sender {
+    NSLog(@"clearViewBtn");
+
+}
+
+@end
